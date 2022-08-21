6d7ab2129c88c955d8a877b9880c20f6d91880b5
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-21 18:13:04 +0300

    Доп задача на массивы - кусты черники

---
+++ ExtraExercise2/Program.cs
@@ -0,0 +1,14 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[n];
+for (int i = 0; i < n; i++)
+{
+    num[i] = Convert.ToInt32(Console.ReadLine());
+}
+int result = num[n - 1] + num[0] + num[1];
+if (result < num[n - 2] + num[n - 1] + num[0]) result = num[n - 2] + num[n - 1] + num[0];
+for (int i = 1; i < n - 1; i++)
+{
+    int k = num[i - 1] + num[i] + num[i + 1];
+    if (k > result) result = k;
+}
+Console.WriteLine(result);
