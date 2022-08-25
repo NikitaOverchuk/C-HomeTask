c4cf417b98ad181351341a6740a93b2f0d057545
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-25 17:33:18 +0300

    Задача 41

---
+++ sds/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+int[] num = new int[n];
+int res = 0;
+for (int i = 0; i < n; i++)
+{
+    int k = Convert.ToInt32(Console.ReadLine());
+    if (k > 0) res += 1;
+    num[i] = k;
+}
+Console.WriteLine(string.Join(", ", num));
+Console.WriteLine(res);
