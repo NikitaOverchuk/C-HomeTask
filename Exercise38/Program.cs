382bcd7701789aca872ca054aea3375c6164fd8d
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-25 17:26:27 +0300

    Задание 38

---
+++ Exercise38/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = Convert.ToInt32(Console.ReadLine());
+double[] num = new double[n];
+double maxNum = Convert.ToDouble(Console.ReadLine());
+double minNum = maxNum;
+for (int i = 1; i < n; i++)
+{
+    double k = Convert.ToDouble(Console.ReadLine());
+    if (k > maxNum) maxNum = k;
+    if (k < minNum) minNum = k;
+    num[i] = k;
+}
+Console.WriteLine("[" + string.Join(", ", num) + "]");
+Console.WriteLine(maxNum - minNum);
