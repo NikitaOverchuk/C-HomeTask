c3241cb3d03e61371709cf29ba9692e5bad6c728
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-25 17:26:12 +0300

    Задание 36

--- Exercise36/Program.cs
+++ Exercise36/Program.cs
@@ -1,14 +1,9 @@
 ﻿int n = Convert.ToInt32(Console.ReadLine());
 int[] num = new int[n];
-int res = 0;
 for (int i = 0; i < n; i++)
-{
-    //int k = new Random().Next(-10, 11);
-    num[i] = new Random().Next(-10, 11);
-}
-Console.WriteLine("[" + string.Join(", ", num) + "]");
+    num[i] = new Random().Next(0, 11);
+Console.WriteLine(string.Join(", ", num));
+int res = 0;
 for (int i = 1; i < n; i += 2)
-{
     res += num[i];
-}
 Console.WriteLine(res);
