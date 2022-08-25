e447681bb3998ce68f74e7594e3c7360a745f4aa
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-25 17:25:28 +0300

    Задание 34

--- Exercise34/Program.cs
+++ Exercise34/Program.cs
@@ -1,10 +1,10 @@
 ﻿int n = Convert.ToInt32(Console.ReadLine());
-int num = new int[n];
+int[] num = new int[n];
 int count = 0;
 for (int i = 0; i > n; i++)
 {
     int k = new Random().Next(100, 1000);
-    if (k % 2 != 0) count += 1;
+    if (k % 2 == 0) count += 1;
     num[i] = k; 
 }
 Console.WriteLine("[" + string.Join(", ", num) + "]");
