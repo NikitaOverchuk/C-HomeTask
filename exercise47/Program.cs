d3d200e1cfdd39247fd4723e1b976d5581e593cf
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-08-29 22:53:51 +0300

    задача 47

---
+++ exercise47/Program.cs
@@ -0,0 +1,12 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+double[,] num = new double[m, n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        num[i, j] = Convert.ToDouble(new Random().Next(-100, 100)) / 10;
+        Console.Write(num[i, j] + "\t");
+    }
+    Console.WriteLine();
+}
