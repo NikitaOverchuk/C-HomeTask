9d65a8157b42eba19609ba877d8518315a5b89a6
Nikita Overchuk <nik.overchuk.97@mail.ru>
2022-09-08 19:03:42 +0300

    задание 68

---
+++ exercise68/Program.cs
@@ -0,0 +1,16 @@
+﻿int A(int n, int m)
+{
+    if (n == 0) return m + 1;
+    if (n != 0 && m == 0) return A(n - 1, 1);
+    if (n > 0 && m > 0) return A(n - 1, A(n, m - 1));
+    return A(n, m);
+}
+
+
+
+Console.WriteLine("Введите число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите  число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.WriteLine(A(n, m));
